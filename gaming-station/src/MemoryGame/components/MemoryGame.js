ea793b1e8d68a3bface038670141d6663297965a
Etvoila <etvoila.jw@gmail.com>
2022-06-26 23:22:52 +1200

    add stages and dynamically reset board according to stage level

--- gaming-station/src/MemoryGame/components/MemoryGame.js
+++ gaming-station/src/MemoryGame/components/MemoryGame.js
@@ -2,16 +2,41 @@ import { useState, useEffect } from "react";
 import Board from "./Board";
 
 const MemoryGame = () => {
-  const [game, setGame] = useState();
   const [reset, setReset] = useState(true);
+  const [board, setBoard] = useState();
+  const [currentStage, setCurrentStage] = useState(0);
+  const [stages, setStages] = useState([1, 2, 3]);
 
-  //  const startGame = () => {
-  //    setGame();
-  //  };
+  useEffect(() => {
+    makeBoard();
+  }, [reset, currentStage]);
+
+  const makeBoard = () => {
+    let col = stages[currentStage] + 2;
+    let row = stages[currentStage] + 2;
+    let selectedNum = stages[currentStage] + 1;
+    setBoard(
+      <Board col={col} row={row} selectedNum={selectedNum} reset={reset} />
+    );
+  };
+
+  const changeStage = (e) => {
+    console.log(e.target.dataset["mssg"]);
+    setCurrentStage(e.target.dataset["mssg"] - 1);
+    setReset(!reset);
+  };
+
   return (
     <>
       <button onClick={() => setReset(!reset)}>Start Game</button>
-      <Board col={3} row={3} selectedNum={2} reset={reset} />
+      {board}
+      {stages.map((stage) => {
+        return (
+          <button data-mssg={stage} onClick={changeStage}>
+            {stage}
+          </button>
+        );
+      })}
     </>
   );
 };
